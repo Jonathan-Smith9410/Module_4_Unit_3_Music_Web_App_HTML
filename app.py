ffbc7c7ea31e03c57ebd31b4b1a08e45d8a493ca
Jonathan-Smith9410 <jsmith300599@googlemail.com>
2026-01-08 15:18:45 +0000

    Project setup

--- app.py
+++ app.py
@@ -1,11 +1,60 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.album_repository import AlbumRepository
+from lib.album import Album
+from lib.artist_repository import ArtistRepository
+from lib.artist import *
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/albums', methods=["POST"])
+def post_albums():
+    if 'title' not in request.form or 'release_year' not in request.form or 'artist_id' not in request.form:
+        return "Necessary album data expected", 400
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    album = Album(
+        None,
+        request.form['title'],
+        request.form['release_year'],
+        request.form['artist_id']
+        )
+    repository.create(album)
+    return '', 200
+
+
+@app.route('/albums', methods=["GET"])
+def get_albums():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    albums = repository.all()
+    return "\n".join(
+        f"{album}" for album in albums
+    )
+
+
+@app.route('/artists', methods=["GET"])
+def get_artists():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    artists = repository.get_artists_as_string()
+    return artists
+
+@app.route('/artists', methods=["POST"])
+def post_artist():
+    if 'name' not in request.form or 'genre' not in request.form:
+        return "You need to submit a name and genre", 400
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    artist = Artist(
+        None,
+        request.form['name'],
+        request.form['genre'])
+    repository.create(artist)
+    return "", 200
 
 
 # == Example Code Below ==
